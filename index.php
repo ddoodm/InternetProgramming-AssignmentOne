b50b8d8a268ccaab9d839e2cb827c794bcaf9e20
Deinyon Davies <deinyond@gmail.com>
2016-04-05 17:11:42 +1000

    Preliminary design prototype and implementation

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="Style/main.css"/>
+
+    <title>Interprog Travel</title>
+  </head>
+
+  <body>
+  	<!-- Head Spacer -->
+  	<div id="headBar">
+  		<img id="headerLogo" src="Style/UI/ITLogo.png" />
+  	</div>
+
+  	<!-- Navigation Bar -->
+  	<div id="navBar"></div>
+
+  	<!-- Main Body -->
+  	<div id="bodyWrapper">
+  		<!-- Footer -->
+  		<div id="footer">&nbsp;</div>
+  	</div>
+
+  </body>
+</html>
