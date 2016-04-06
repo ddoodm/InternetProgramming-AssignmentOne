9efa3724202a7b1398584674e77a5636cb3edf3e
Deinyon Davies <deinyond@gmail.com>
2016-04-06 12:05:55 +1000

    Style update, including new info panel

--- index.php
+++ index.php
@@ -22,16 +22,25 @@
   	<!-- Main Body -->
   	<div id="bodyWrapper">
       <div id="content">
-        <h1>Your Journey Begins Here</h1>
+        <h2>Start planning your next adventure today, with Interprog Travel!</h2>
 
         <!-- Promo Image -->
         <img class="fullBodyPromoImage" src="Images/PromoPic_00.jpg">
 
+        <div class="infoBox">
+          <h3>
+            Book your flight now for the best deals !<br />
+            Don't wait !
+          </h3>
+          <button class="lumpyButton">
+            Book Now
+          </button>
+        </div>
+
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dolor ante, laoreet in metus eu, pharetra ultrices ante. Suspendisse non interdum orci. Sed accumsan mi ut erat pulvinar congue. Nullam neque ipsum, porta non fringilla sed, egestas at quam. Donec hendrerit ante in rutrum pellentesque. Aliquam a quam a lectus placerat consectetur. Sed a mauris at erat maximus lobortis luctus vitae urna. Donec a lorem a diam vestibulum sagittis. Vestibulum at mi diam. Fusce non diam convallis, malesuada quam et, tristique lacus. Aliquam erat volutpat. Maecenas suscipit tempus turpis, ac pellentesque eros fermentum at.
         </p>
-
-        <p>
+        <p class="rightJustify">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dolor ante, laoreet in metus eu, pharetra ultrices ante. Suspendisse non interdum orci. Sed accumsan mi ut erat pulvinar congue. Nullam neque ipsum, porta non fringilla sed, egestas at quam. Donec hendrerit ante in rutrum pellentesque. Aliquam a quam a lectus placerat consectetur. Sed a mauris at erat maximus lobortis luctus vitae urna. Donec a lorem a diam vestibulum sagittis. Vestibulum at mi diam. Fusce non diam convallis, malesuada quam et, tristique lacus. Aliquam erat volutpat. Maecenas suscipit tempus turpis, ac pellentesque eros fermentum at.
         </p>
       </div>
